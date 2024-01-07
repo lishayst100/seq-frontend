03b517176613ed02b8251c4f6f6f15c3b4e46865
lishayst100 <lishayst100@gmail.com>
2024-01-07 14:50:45 +0200

    update server url

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3001/api/projects';
-//export const BASE_URL = 'https://server-seq.onrender.com/api/projects';
+//export const BASE_URL = 'http://localhost:3001/api/projects';
+export const BASE_URL = 'https://server-seq.onrender.com/api/projects';
 
 //https://server-seq.onrender.com/
