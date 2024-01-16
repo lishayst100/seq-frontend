561eacea08e4b62825d1ca74937abad5959a7a1f
lishayst100 <lishayst100@gmail.com>
2024-01-16 17:27:51 +0200

    carousel from server

--- src/components/Contact/Carusel/Carusela.jsx
+++ src/components/Contact/Carusel/Carusela.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Carousel from 'react-bootstrap/Carousel';
 import logo from '../../../images/logo.png'
 import { silders } from './carouselImages';
 import { useResize } from '../../../hooks/useResize';
+import { useState } from 'react';
+import { BASE_URL } from '../../../utils/utils';
 
 
 const Carusela = () => {
     const resize = useResize()
+    const [images, setImages] = useState([])
+
+    useEffect(()=>{
+        fetch(`${BASE_URL}/carousel/`)
+        .then(res => res.json())
+        .then(result => setImages(result[0].url))
+    },[])
   return (
     <div className='carousel'>
         <Carousel fade>
         {
-            silders.map(slider => (
+            images.map(slider => (
                 <Carousel.Item key={slider}>
                     <img src={slider} alt='...' style={{height:resize > 800 ? 450 : 200, width:'100%', objectFit:'cover'}}/>
                 </Carousel.Item>
