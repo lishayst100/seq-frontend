7faed0a8ebca1c987ca70e819cbc84fef86dd3f1
lishayst100 <lishayst100@gmail.com>
2024-01-21 18:14:10 +0200

    with uence

--- src/components/Home/Logo/LogoSeq.jsx
+++ src/components/Home/Logo/LogoSeq.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Seq from "../../../images/SEQ (2).png";
 import asterisk from "../../../images/asterisk (1).png";
-
+import uence from '../../../images/UENCE.png'
 import { motion, useTransform } from "framer-motion";
 
 
@@ -13,14 +13,11 @@ const LogoSeq = ({scrollYProgress}) => {
    
 
   return (
-    <motion.div className="pt-5 d-flex gap-3 sequence-logo " >
-     
-
-
-
+    <motion.div className="pt-5 d-flex gap-3 sequence-logo" >
       <img src={Seq} alt="..." className=" seq "/>
       <motion.img src={asterisk} alt="..." className="asterisk" style={{rotate}}/>
       <motion.div className=" d-flex  uence " style={{scale,translateX}} >
+          <img src={uence} alt="..." />
       </motion.div>
     </motion.div>
   );
