63066a9c14ce1015d908cd570b1c045baece9434
lishayst100 <lishayst100@gmail.com>
2024-01-21 18:26:37 +0200

    uence update

--- src/components/Home/Logo/LogoSeq.jsx
+++ src/components/Home/Logo/LogoSeq.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Seq from "../../../images/SEQ (2).png";
 import asterisk from "../../../images/asterisk (1).png";
-import uence from '../../../images/UENCE.png'
+import uence from '../../../images/UENCE (1).png'
 import { motion, useTransform } from "framer-motion";
 
 
