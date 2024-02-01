a8e442bb502823f93b713caced6b52bc7655882a
lishayst100 <lishayst100@gmail.com>
2024-02-01 17:37:22 +0200

    video fixex

--- src/components/Home/Video/Video.jsx
+++ src/components/Home/Video/Video.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ProjectContext } from '../../../context/ProjectContext'
 
 const Video = () => {
     const video = 'https://res.cloudinary.com/dabvgy03h/video/upload/v1704022256/g5j9ucuyql3jhm8uid9z.mp4'
+    const {projects} = useContext(ProjectContext)
   return (
-    <div className='video-container' style={{zIndex: 3 , position: 'relative'}}>
+    <div className='video-container'>
       <video
         className='video'
         src={video}
@@ -11,6 +13,8 @@ const Video = () => {
         muted
         autoPlay
         loop
+        playsInline
+        poster={projects[0]?.images[0]}
       />
      
     </div>
